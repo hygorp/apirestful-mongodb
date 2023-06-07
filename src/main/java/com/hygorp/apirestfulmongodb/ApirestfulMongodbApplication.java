52634a3863a7577f98f4a99b36027d3af621002c
Hygor Henrique <hygor.henrique@live.com>
2023-06-07 10:24:42 -0300

    Initial Commit

---
+++ src/main/java/com/hygorp/apirestfulmongodb/ApirestfulMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.hygorp.apirestfulmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfulMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfulMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hygorp/apirestfulmongodb/ApirestfulMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hygorp.apirestfulmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
