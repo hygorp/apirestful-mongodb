11650e60d11bc48a085b6e277eb417f4429ec9b4
Hygor Henrique <hygor.henrique@live.com>
2023-06-07 19:33:48 -0300

    get post by id endpoint

---
+++ src/main/java/com/hygorp/apirestfulmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.hygorp.apirestfulmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hygorp.apirestfulmongodb.domain.Post;
+import com.hygorp.apirestfulmongodb.repositories.PostRepository;
+import com.hygorp.apirestfulmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+}
